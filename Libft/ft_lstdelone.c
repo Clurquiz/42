e6b1592ecb42ab00084e48bc8278d801df552366
Clurquiz <clementine.urquizar@gmail.com>
2016-11-17 13:34:18 +0100

    ft_lstdelone : fonction finie

--- Libft/ft_lstdelone.c
+++ Libft/ft_lstdelone.c
@@ -2,5 +2,9 @@
 
 void	ft_lstdelone(t_list **alst, void (*del)(void *, size_t))
 {
-	
+	(*del)(*alst->content);
+	(*del)(*alst->content_size);
+	(*del)(*alst->next);
+	free(*alst);
+	*alst = NULL;
 }
