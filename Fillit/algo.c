7dd6ffcc56189f86c61d1bb0966cfe1f60d48e20
Clementine URQUIZAR <curquiza@e2r6p5.42.fr>
2016-11-25 19:21:24 +0100

    ft_check_and_place en cours

--- Fillit/algo.c
+++ Fillit/algo.c
@@ -6,13 +6,13 @@
 /*   By: baparis <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/25 10:44:30 by baparis           #+#    #+#             */
-/*   Updated: 2016/11/25 15:55:46 by curquiza         ###   ########.fr       */
+/*   Updated: 2016/11/25 19:21:00 by curquiza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-int	ft_check_and_place(char **endgrid, t_list lst)
+/*int	ft_check_and_place(char **endgrid, t_list lst)
 {
 	int	j;
 
@@ -23,45 +23,68 @@ int	ft_check_and_place(char **endgrid, t_list lst)
 		return (1);
 	(void)lst;
 	return (0);
-}
+}*/
 
 // CLEM
-/*int		ft_check_case(char **endgrid, int i, int j)
+int		ft_check_case(char **endgrid, int i, int j)
 {
-	if (endgrid[i][j] == '$' || endgrid[i][j] == '#' || endgrid[i][j] == '\0')
+	if (endgrid[i][j] == '?' || (endgrid[i][j] >= 'A' && endgrid[i][j] <= 'Z')
+		|| endgrid[i][j] == '\0')
 		return (0);
 	return (1);
 }
 
-int		ft_check_a_position(char **endgrid, t_list *lst, i, j)
+int		ft_check_a_position(char **endgrid, t_list lst, int i, int j)
 {
 	int		k;
-	int		l;
+	int		tmp_k;
+	int		cpt_block;
 
+	cpt_block = 0;
 	k = 0;
-	while ((lst->tetri2d)[k])
+	if (ft_check_case(endgrid, i, j) == 0)
+		return (0);
+	while (*(lst.tetri1d))
 	{
-		l = 0;
-		while ((lst->tetri2d)[k][l])
+		if (*(lst.tetri1d) == '#')
 		{
-			l++;
+			cpt_block++;
+			if (cpt_block == 1)
+			{
+				endgrid[i][j] = lst.letter;
+				tmp_k = k;
+			}	
+			else
+			{
+				if (ft_check_case(endgrid, i + (k / 4 - tmp_k / 4), j + (k % 4 - tmp_k % 4) == 0))
+					return (0);
+				endgrid[i + (k / 4 - tmp_k / 4)][j + (k % 4 - tmp_k % 4)] = lst.letter;
+			}
 		}
-		
-		k++;
+		if (*(lst.tetri1d) != '\n')
+			k++;
+		(lst.tetri1d)++;
 	}
+	return (1);
 }
 
-
-int		ft_check_and_place(char **endgrid, t_list *lst)
+int		ft_check_and_place(char **endgrid, t_list lst, int i, int j)
 {
-	int		i;
-	int		j;
-
-	i = 0;
-	j = 0;
-
-}*/
-
+	if (endgrid[i + 2] == 0 && endgrid[i][j] == '\0')
+		return (0);
+	else
+	{
+		j++;
+		if (endgrid[i][j] == '\0')
+		{
+			j = 1;
+			i++;
+		}
+		if (ft_check_and_place(endgrid, lst, i, j) == 1)
+			return (1);
+	}
+	return (0);
+}
 
 
 /*
@@ -102,7 +125,7 @@ int		ft_algo(t_list *lst, char **endgrid)
 	{
 		while (tmp->used == 1 && tmp)
 			tmp = tmp->next;
-		if (ft_check_and_place(endgrid, *tmp))
+		if (ft_check_and_place(endgrid, *tmp, 0, 1))
 		{
 			tmp->used = 1;
 			if (!(ft_algo(lst, endgrid)))
--- Fillit/fillit.h
+++ Fillit/fillit.h
@@ -6,7 +6,7 @@
 /*   By: baparis <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/23 12:02:40 by baparis           #+#    #+#             */
-/*   Updated: 2016/11/25 18:10:54 by curquiza         ###   ########.fr       */
+/*   Updated: 2016/11/25 19:16:34 by curquiza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,7 +67,7 @@ t_list				*ft_create_elem(char *tetri1d, int num);
 t_list				*ft_fill_lst(char *av);
 
 // FCT DE ALGO.C
-int					ft_check_and_place(char **endgrid, t_list lst);
+int					ft_check_and_place(char **endgrid, t_list lst, int i, int j);
 void				ft_erase(t_list *lst, char **endgrid);
 int					ft_algo(t_list *lst, char **endgrid);
 
