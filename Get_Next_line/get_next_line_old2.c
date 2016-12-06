47838ff2541a89aa018cfbe545b6579652e17bba
Clementine URQUIZAR <curquiza@e2r4p2.42.fr>
2016-12-06 13:24:18 +0100

    recommence

--- Get_Next_line/get_next_line_old2.c
+++ Get_Next_line/get_next_line_old2.c
@@ -6,75 +6,83 @@
 /*   By: curquiza <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/04 17:34:05 by curquiza          #+#    #+#             */
-/*   Updated: 2016/12/06 13:23:16 by curquiza         ###   ########.fr       */
+/*   Updated: 2016/12/05 18:21:28 by curquiza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-// peut etre pas top car *line n'est pas remis a NULL
-// pour palier a ça : faire un tmp qui prend la valeur de line (pas l'adresse je suppose) par ex avec un strdup
-// puis free et mettre a NULL line (= strdel ?)
-// puis faire *line = strjoin(tmp, buff)
 void	ft_realloc_and_fill(char **dst, char *buff)
 {
+	char	*tmp;
+
 	if (dst && buff)
 	{
 		if (*dst == NULL)
 			*dst = ft_strdup(buff);
 		else
 		{
-			free(*dst);
-			*dst = ft_strjoin(*dst, buff);
+			tmp = ft_strdup(*dst);
+			ft_strdel(dst);
+			*dst = ft_strjoin(tmp, buff);
+			ft_strdel(&tmp);
 		}
 	}
 }
 
-int		ft_check_newline(char *tmp)
+int		ft_fill_line(t_file *file, char **line)
 {
-	while (tmp)
+	char	*tmp;
+	int		cpt;
+	int		start;
+	int		i;
+
+	cpt = 0;
+	start = 0;
+	i = 0;
+	while (file->content[i] && cpt <= file->row)
 	{
-		if (*tmp == '\n')
-			return (1);
-		tmp++;
+		if (cpt == file->row - 1)
+		{
+			start = i;
+			cpt++;
+		}
+		if (file->content[i] == '\n')
+			cpt++;
+		i++;
 	}
-	return (0);
+	tmp = ft_strsub(file->content, start, i - start);
+	ft_realloc_and_fill(line, tmp);
+	ft_strdel(&tmp);
+	if (file->content[i] == '\0')
+		return (0);
+	else
+		return (1);
 }
 
-void	ft_keep_line(char *tmp)
+void	ft_init_file(t_file *file, int fd)
 {
-	if (tmp)
-	{
-		while (tmp && *tmp != '\n')
-			tmp++;
-		if (*tmp == '\n')
-			tmp++;
-		*tmp = '\0';
-	}
+	file->content = NULL;
+	file->fd = fd;
+	file->row = 0;
 }
 
 int		get_next_line(const int fd, char **line)
 {
-	int				i;
 	char			buff[BUFF_SIZE + 1];
-	char static		*tmp_line;
 	int				ret;
+	static t_file	file;
 
-	i = 0;
 	ret = 0;
-	//tmp_line = NULL;
+	(file.row)++;
 	while ((ret = read(fd, buff, BUFF_SIZE)) > 0)
 	{
 		buff[ret] = '\0';
-		ft_realloc_and_fill(&tmp_line, buff);
-		//if (ft_check_newline(buff) == 1)
-		//	break;
+		ft_realloc_and_fill(&(file.content), buff);
 	}
-	printf("tmp_lin = %s\n", tmp_line);
-	ft_keep_line(tmp_line);
-	printf("tmp_lin = %s\n", tmp_line);
-	ft_realloc_and_fill(line, tmp_line);
-	//free(tmp_line);
-
-	return (0);
+	if (ret < 0)
+		return (-1);
+	if (file.content == NULL) //cas du fichier vide
+		file.content = ft_strdup("\0");
+	return (ft_fill_line(&file, line));
 }
