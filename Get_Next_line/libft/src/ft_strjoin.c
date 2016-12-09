c180ab12dd278ed13e98ff0f6c89d851d6213915
Clementine URQUIZAR <curquiza@e2r4p2.42.fr>
2016-12-09 16:53:37 +0100

    modif de ft_strjoin, prise en compte plus fine du cas NULL

--- Get_Next_line/libft/src/ft_strjoin.c
+++ Get_Next_line/libft/src/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: curquiza <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/15 11:16:01 by curquiza          #+#    #+#             */
-/*   Updated: 2016/11/18 14:59:26 by curquiza         ###   ########.fr       */
+/*   Updated: 2016/12/09 16:41:53 by curquiza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,28 +15,19 @@
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*tmp;
-	int		len_s1;
-	int		len_s2;
 
-	if (!s1 || !s2)
+	if (!s1 && !s2)
 		return (NULL);
-	len_s1 = ft_strlen(s1);
-	len_s2 = ft_strlen(s2);
-	if (!(tmp = (char *)malloc(sizeof(*tmp) * (len_s1 + len_s2 + 1))))
+	if (!s1)
+		return (ft_strdup(s2));
+	if (!s2)
+		return (ft_strdup(s1));
+	if (!(tmp = (char *)malloc(sizeof(*tmp) * (ft_strlen(s1)
+												+ ft_strlen(s2) + 1))))
 		return (NULL);
-	while (*s1)
-	{
-		*tmp = *s1;
-		s1++;
-		tmp++;
-	}
-	while (*s2)
-	{
-		*tmp = *s2;
-		s2++;
-		tmp++;
-	}
-	*tmp = '\0';
-	tmp = tmp - (len_s1 + len_s2);
+	ft_strcpy(tmp, s1);
+	tmp = tmp + ft_strlen(s1);
+	ft_strcpy(tmp, s2);
+	tmp = tmp - (ft_strlen(s1));
 	return (tmp);
 }
