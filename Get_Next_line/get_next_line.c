2aedd135c95822754e1622ad1960345ae5e16a87
Clementine URQUIZAR <curquiza@e2r4p2.42.fr>
2016-12-09 17:19:48 +0100

    GNL a la norme et qui fonctionne

--- Get_Next_line/get_next_line.c
+++ Get_Next_line/get_next_line.c
@@ -6,27 +6,36 @@
 /*   By: curquiza <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/06 13:24:27 by curquiza          #+#    #+#             */
-/*   Updated: 2016/12/09 16:53:05 by curquiza         ###   ########.fr       */
+/*   Updated: 2016/12/09 17:18:38 by curquiza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-void	ft_realloc(char **dst, char *src)
+int		ft_use_buff(char **line, char *buff, char **endbuff)
 {
-	char	*tmp;
+	int		i;
+	char	*tmp_sub;
+	char	*tmp_line;
 
-	if (dst && src)
+	if (ft_strchr(buff, '\n'))
 	{
-		if (*dst == NULL)
-			*dst = ft_strdup(src);
-		else
-		{
-			tmp = *dst;
-			*dst = ft_strjoin(*dst, src);
-			ft_strdel(&tmp);
-		}
+		i = 0;
+		while (buff[i] != '\n')
+			i++;
+		tmp_sub = ft_strsub(buff, 0, i);
+		tmp_line = *line;
+		*line = ft_strjoin(tmp_line, tmp_sub);
+		ft_strdel(&tmp_line);
+		ft_strdel(&tmp_sub);
+		ft_strdel(endbuff);
+		*endbuff = ft_strdup(ft_strchr(buff, '\n') + 1);
+		return (1);
 	}
+	tmp_line = *line;
+	*line = ft_strjoin(tmp_line, buff);
+	ft_strdel(&tmp_line);
+	return (0);
 }
 
 int		ft_use_endbuff(char **endbuff, char **line)
@@ -34,8 +43,6 @@ int		ft_use_endbuff(char **endbuff, char **line)
 	int		i;
 	char	*tmp;
 
-	//if (!line)
-	//	return (0);
 	*line = NULL;
 	if (!(*endbuff) || !(**endbuff))
 		return (0);
@@ -59,9 +66,7 @@ int		get_next_line(const int fd, char **line)
 {
 	static char	*endbuff;
 	char		buff[BUFF_SIZE + 1];
-	char		*tmp;
 	int			ret;
-	int			i;
 
 	if (!line)
 		return (-1);
@@ -70,20 +75,8 @@ int		get_next_line(const int fd, char **line)
 	while ((ret = read(fd, buff, BUFF_SIZE)) > 0)
 	{
 		buff[ret] = '\0';
-		if (ft_strchr(buff, '\n'))
-		{
-			i = 0;
-			while (buff[i] != '\n')
-				i++;
-			tmp = ft_strsub(buff, 0, i);
-			ft_realloc(line, tmp);
-			ft_strdel(&tmp);
-			ft_strdel(&endbuff);
-			endbuff = ft_strdup(ft_strchr(buff, '\n') + 1);
-			//ft_realloc(&endbuff, ft_strchr(buff, '\n') + 1);
+		if (ft_use_buff(line, buff, &endbuff) == 1)
 			return (1);
-		}
-		ft_realloc(line, buff);
 	}
 	if (ret < 0)
 		return (-1);
