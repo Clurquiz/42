5ec4dd218124d05523540b0e89cf97f9a3918c20
Clementine URQUIZAR <curquiza@e2r5p3.42.fr>
2016-12-10 20:35:23 +0100

    sauvegarde

---
+++ Get_Next_line/ok_okmultfd_oknorm_reduit2/get_next_line.c
@@ -0,0 +1,92 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: curquiza <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/12/06 13:24:27 by curquiza          #+#    #+#             */
+/*   Updated: 2016/12/10 20:09:24 by curquiza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+t_file	*ft_find_or_create(t_file **begin, int fd)
+{
+	t_file	*new;
+	t_file	*current;
+
+	if (!(new = (t_file *)malloc(sizeof(*new))))
+		return (NULL);
+	new->fd = fd;
+	new->endbuff = NULL;
+	new->next = NULL;
+	if (*begin == NULL)
+		*begin = new;
+	current = *begin;
+	while (current && fd != current->fd)
+		current = current->next;
+	if (current == NULL)
+	{
+		current = *begin;
+		while (current->next)
+			current = current->next;
+		current->next = new;
+		return (current->next);
+	}
+	return (current);
+}
+
+int		ft_work_on_buffers(char *buff, char **endbuff, char **line)
+{
+	int		i;
+	char	*tmp_sub;
+	char	*tmp_line;
+
+	if (!buff)
+		return (0);
+	i = 0;
+	while (buff[i] && buff[i] != '\n')
+		i++;
+	tmp_sub = ft_strsub(buff, 0, i);
+	tmp_line = *line;
+	*line = ft_strjoin(tmp_line, tmp_sub);
+	ft_strdel(&tmp_line);
+	ft_strdel(&tmp_sub);
+	ft_strdel(endbuff);
+	if (ft_strchr(buff, '\n'))
+	{
+		*endbuff = ft_strdup(ft_strchr(buff, '\n') + 1);
+		return (1);
+	}
+	if (**line == '\0')
+		*line = NULL;
+	return (0);
+}
+
+int		get_next_line(const int fd, char **line)
+{
+	static t_file	*file;
+	t_file			*current;
+	char			buff[BUFF_SIZE + 1];
+	int				ret;
+
+	if (!line)
+		return (-1);
+	current = ft_find_or_create(&file, fd);
+	*line = NULL;
+	if (ft_work_on_buffers(current->endbuff, &(current->endbuff), line) == 1)
+		return (1);
+	while ((ret = read(current->fd, buff, BUFF_SIZE)) > 0)
+	{
+		buff[ret] = '\0';
+		if (ft_work_on_buffers(buff, &(current->endbuff), line) == 1)
+			return (1);
+	}
+	if (ret < 0)
+		return (-1);
+	if (*line != NULL)
+		return (1);
+	return (0);
+}
