a0a9f8b8742f9d8804c7f841ab13635083f0417d
Clementine URQUIZAR <curquiza@e2r4p2.42.fr>
2016-12-11 17:39:03 +0100

    supp de sauvegarde

---
+++ Get_Next_line/newMethode/get_next_line.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: curquiza <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/12/04 17:35:34 by curquiza          #+#    #+#             */
+/*   Updated: 2016/12/11 17:37:47 by curquiza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# define BUFF_SIZE 10
+
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include "libft.h"
+
+typedef struct		s_file
+{
+	int				fd;
+	char			*buff;
+	struct s_file	*next;
+}					t_file;
+
+t_file				*ft_find_or_create(t_file **begin, int fd);
+int					ft_use_buff(char **line, char *buff, char **endbuff);
+int					ft_use_endbuff(char **endbuff, char **line);
+int					get_next_line(const int fd, char **line);
+
+#endif
