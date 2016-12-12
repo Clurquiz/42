cb0480ca78df104cc1e1926e0cd1ca02364d303d
Clementine URQUIZAR <curquiza@e2r4p2.42.fr>
2016-12-12 15:30:57 +0100

    modif ft_strsub.c

--- Get_Next_line/libft/src/ft_strsub.c
+++ Get_Next_line/libft/src/ft_strsub.c
@@ -6,7 +6,7 @@
 /*   By: curquiza <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/14 18:27:32 by curquiza          #+#    #+#             */
-/*   Updated: 2016/11/18 14:40:35 by curquiza         ###   ########.fr       */
+/*   Updated: 2016/12/12 15:30:21 by curquiza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,12 @@ char	*ft_strsub(char const *s, unsigned int start, size_t len)
 	if (!s)
 		return (NULL);
 	if (!(tmp = (char *)malloc(sizeof(*tmp) * (len + 1))))
+	if (!(tmp = ft_strnew(len)))
 		return (NULL);
 	while (s[start + i] != '\0' && i < len)
 	{
 		tmp[i] = s[start + i];
 		i++;
 	}
-	tmp[len] = '\0';
 	return (tmp);
 }
