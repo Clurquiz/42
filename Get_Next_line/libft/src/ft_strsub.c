348e10e4393f929f9c86a36223f0411aafd916e4
Clementine URQUIZAR <curquiza@e2r4p2.42.fr>
2016-12-12 15:41:55 +0100

    corr

--- Get_Next_line/libft/src/ft_strsub.c
+++ Get_Next_line/libft/src/ft_strsub.c
@@ -6,7 +6,7 @@
 /*   By: curquiza <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/14 18:27:32 by curquiza          #+#    #+#             */
-/*   Updated: 2016/12/12 15:30:21 by curquiza         ###   ########.fr       */
+/*   Updated: 2016/12/12 15:36:51 by curquiza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ char	*ft_strsub(char const *s, unsigned int start, size_t len)
 	i = 0;
 	if (!s)
 		return (NULL);
-	if (!(tmp = (char *)malloc(sizeof(*tmp) * (len + 1))))
+	//if (!(tmp = (char *)malloc(sizeof(*tmp) * (len + 1))))
 	if (!(tmp = ft_strnew(len)))
 		return (NULL);
 	while (s[start + i] != '\0' && i < len)
@@ -28,5 +28,6 @@ char	*ft_strsub(char const *s, unsigned int start, size_t len)
 		tmp[i] = s[start + i];
 		i++;
 	}
+	//tmp[len] = '\0';
 	return (tmp);
 }
