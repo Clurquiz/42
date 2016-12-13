6fdf8db145878c71999ec1fc0f3eab1dd9008e94
Clementine URQUIZAR <curquiza@e2r5p10.42.fr>
2016-12-13 15:59:49 +0100

    maj du main.c

--- Get_Next_line/main.c
+++ Get_Next_line/main.c
@@ -8,16 +8,19 @@ int		main(int ac, char **av)
 	char	*line;
 	int		fd;
 	int		ret;
-	int		cpt;
 
-	cpt = 0;
-	if (ac != 2)
+	if (ac > 2)
 		return (0);
-	
-	//line = ft_strdup("ecole42 => ");
+	if (ac == 1)
+		fd = 0;
+	else
+		fd = open(av[1], O_RDONLY);
+
 	line = NULL;
 
-	fd = open(av[1], O_RDONLY);
+/*
+** test sur 5 lignes sans risque de boucle infinie
+*/
 
 	/*ret = get_next_line(fd, &line);
 	printf("ret = %d\n", ret);
@@ -50,19 +53,27 @@ int		main(int ac, char **av)
 	ft_strdel(&line);
 	printf("--------------------------\n");*/
 
-	
+
+/*	
+** test sur tout le fichier avec valeurs de retour
+*/
+
 	while ((ret = get_next_line(fd, &line)) >  0)
 	{	
 		printf("ret = %d\n", ret);
 		printf("line : %s\n", line);
 		//freeeline);
-		//ft_strdel(&line);
+		ft_strdel(&line);
 		printf("------------------------\n");
 	}
 	printf("ret = %d\n", ret);
 	printf("line : %s\n", line);
 	//free(line);
-	//ft_strdel(&line);
+	ft_strdel(&line);
+
+/*
+** test sur tout le fichier avec uniquement l'affichauge du fichier
+*/
 
 	/*while ((ret = get_next_line(fd, &line)) >  0)
 	{
@@ -70,16 +81,7 @@ int		main(int ac, char **av)
 		ft_strdel(&line);
 	}*/
 	
-	/*while (cpt < 5)
-	{
-		ret = get_next_line(fd, &line);
-		printf("ret = %d\n", ret);
-		printf("%s\n", line);
-		printf("------------------\n");
-		cpt++;
-	}*/
+	close(fd);
 
-	if (close(fd) == -1)
-		return (0);
 	return (0);
 }
