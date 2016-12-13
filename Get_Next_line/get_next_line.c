b47de538e8e9e90beeebfe74df982fbfefe7d678
Clementine URQUIZAR <curquiza@e2r5p10.42.fr>
2016-12-13 16:59:26 +0100

    MAJ

--- Get_Next_line/get_next_line.c
+++ Get_Next_line/get_next_line.c
@@ -6,39 +6,11 @@
 /*   By: curquiza <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/06 13:24:27 by curquiza          #+#    #+#             */
-/*   Updated: 2016/12/12 18:19:41 by curquiza         ###   ########.fr       */
+/*   Updated: 2016/12/13 16:57:59 by curquiza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <stdio.h>
-
-/*t_file	*ft_find_or_create(t_file **begin, int fd)
-{
-	t_file	*new;
-	t_file	*current;
-
-	if (!(new = (t_file *)malloc(sizeof(*new))))
-		return (NULL);
-	new->fd = fd;
-	if (!(new->buff = ft_strnew(BUFF_SIZE)))
-		return (NULL);
-	new->next = NULL;
-	if (*begin == NULL)
-		*begin = new;
-	current = *begin;
-	while (current && fd != current->fd)
-		current = current->next;
-	if (current == NULL)
-	{
-		current = *begin;
-		while (current->next)
-			current = current->next;
-		current->next = new;
-		return (current->next);
-	}
-	return (current);
-}*/
 
 t_file	*ft_create_elem(int fd)
 {
@@ -57,6 +29,8 @@ t_file	*ft_find_or_create(t_file **begin, int fd)
 {
 	t_file	*current;
 
+	if (fd < 0)
+		return (NULL);
 	if (*begin == NULL)
 	{
 		*begin = ft_create_elem(fd);
@@ -76,45 +50,50 @@ t_file	*ft_find_or_create(t_file **begin, int fd)
 	return (current);
 }
 
+void	ft_remove_file(t_file **begin, int fd)
+{
+	t_file	*current;
+	t_file	*supp;
+
+	current = *begin;
+	if (current->fd == fd)
+	{
+		ft_strdel(&(current->buff));
+		current->fd = -1;
+		*begin = (*begin)->next;
+		free(current);
+	}
+	else
+	{
+		while (current->next && current->next->fd != fd)
+			current = current->next;
+		supp = current->next;
+		current->next = current->next->next;
+		ft_strdel(&(supp->buff));
+		supp->fd = -1;
+		supp->next = NULL;
+		free(supp);
+	}
+}
+
 int		ft_work_with_buff(char *buff, char **line)
 {
 	int		i;
 	int		start;
 	char	*tmp_sub;
 	char	*tmp_line;
-	/*int		j;
 
-	j = 0;
-	printf("buff1 = ");
-	while (j <= BUFF_SIZE)
-	{
-		printf("%c", buff[j]);
-		j++;
-	}
-	printf("\n");*/
-	
-	
 	i = -1;
 	start = 0;
 	while (++i < BUFF_SIZE && *(buff) != '\n')
-	{
-		//if (*(buff++) == '\0' && *(buff + 1))
-		//if (*(buff + 1) && *(buff++) == '\0')
-		//if (*(buff + 1) && *(buff) == '\0') //vrai
 		if (*(buff++) == '\0' && *(buff))
 			start = i + 1;
-	//	buff++;
-		//i++;
-	}
 	tmp_sub = ft_strsub(buff - i, start, i - start);
-	//printf("tmp_sub = %s\n", tmp_sub);
-	//printf("i = %d\n", i);
-	//printf("start = %d\n", start);
 	tmp_line = *line;
 	*line = ft_strjoin(tmp_line, tmp_sub);
 	ft_strdel(&tmp_line);
 	ft_strdel(&tmp_sub);
-	if (*buff  == '\n')
+	if (*buff == '\n')
 	{
 		ft_bzero(buff - i, i + 1);
 		return (1);
@@ -125,74 +104,21 @@ int		ft_work_with_buff(char *buff, char **line)
 	return (0);
 }
 
-/*void	ft_remove_file(t_file **begin, int fd)
-{
-	t_file	*current;
-
-	current = *begin;
-	if (current->fd == fd)
-	{
-		ft_strdel(&(current->buff));
-		current->fd = -1;
-		current->next = NULL;
-		free(*begin);
-	}
-	else
-	{
-		while (current->next->fd != fd)
-			current = current->next;
-		ft_strdel(&(current->next->buff));
-		current->next->fd = -1;
-		current->next = current->next->next;
-		current->next->next = NULL;
-		free(current);
-	}
-}*/
-
-void	ft_remove_file(t_file **begin, int fd)
-{
-	t_file	*current;
-	t_file	*supp;
-
-	(void)supp;
-	current = *begin;
-	if (current->fd == fd)
-	{
-		ft_strdel(&(current->buff));
-		current->fd = -1;
-		*begin = (*begin)->next;
-		current->next = NULL;
-		free(current);
-	}
-	/*else
-	{
-		while (current->next && current->next->fd != fd)
-			current = current->next;
-		supp = current->next;
-		current = supp->next;
-		ft_strdel(&(supp->buff));
-		supp->fd = -1;
-		supp->next = NULL;
-		free(supp);
-	}*/
-}
-
 int		get_next_line(const int fd, char **line)
 {
 	static t_file	*file;
 	t_file			*current;
 	int				ret;
 
-	//if (!line)
-	if (!line || fd < 0)
+	if (!line || BUFF_SIZE < 0)
 		return (-1);
-	current = ft_find_or_create(&file, fd);
 	*line = NULL;
+	if (!(current = ft_find_or_create(&file, fd)))
+		return (-1);
 	if (ft_work_with_buff(current->buff, line) == 1)
 		return (1);
 	while ((ret = read(current->fd, current->buff, BUFF_SIZE)) > 0)
 	{
-		//(current->buff)[ret] = '\0';
 		if (ft_work_with_buff(current->buff, line) == 1)
 			return (1);
 	}
--- Get_Next_line/get_next_line.h
+++ Get_Next_line/get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: curquiza <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/04 17:35:34 by curquiza          #+#    #+#             */
-/*   Updated: 2016/12/12 17:56:23 by curquiza         ###   ########.fr       */
+/*   Updated: 2016/12/13 13:19:54 by curquiza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 9999
+# define BUFF_SIZE 42
 
 # include <sys/types.h>
 # include <sys/uio.h>
@@ -28,7 +28,9 @@ typedef struct		s_file
 	struct s_file	*next;
 }					t_file;
 
+t_file				*ft_create_elem(int fd);
 t_file				*ft_find_or_create(t_file **begin, int fd);
+void				ft_remove_file(t_file **begin, int fd);
 int					ft_work_with_buff(char *buff, char **line);
 int					get_next_line(const int fd, char **line);
 
